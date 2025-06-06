e45d6d16e72f019facdb8104054514c2966fa86e
Luiz Henrique Silva Andrade <82455774+LuizHSA@users.noreply.github.com>
2025-06-06 04:00:28 -0300

    Initial commit

---
+++ Iniciante/URI 1012.py
@@ -0,0 +1,40 @@
+#def zero():
+#     print('Hello World!')
+    #
+#zero()
+
+# def um():
+    
+#     print(f'X = {int(input()) + int(input())}')    
+# um()
+
+# def dois():
+    
+#     print(f'A={3.14159*float(input())**2:.4f}')
+# dois()
+
+# def tres():
+#     a, b = int(input()), int(input())
+#     print(f'SOMA = {a+b}')
+# tres()
+
+# def quatro():
+#   a,b = int(input()), int(input())
+#   print(f'PROD = {a*b}')
+# quatro()    
+
+# def cinco():    
+#     a, b = float(input()), float(input())
+#     print(f'MEDIA = {(a*3.5+b*7.5)/11:.5f}')
+# cinco()    
+
+# def seis():
+#     a, b, c = float(input()), float(input()), float(input())
+#     print(f'MEDIA = {(a*2+b*3+c*5)/10:.1f}')
+# seis()
+
+# def sete():
+#     a,b,c,d = int(input()), int(input()), int(input()), int(input())
+#     print(f'DIFERENCA = {a*b-c*d}')
+# sete()    
+
