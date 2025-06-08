00ac2230183489b46433e1a609ccf3e477e45791
Luiz Henrique Silva Andrade <Luiz.andradesilva03@gmail.com>
2025-06-08 00:39:04 -0300

    Update SQL.sql

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -15,3 +15,15 @@ SELECT id, name
 FROM products
 WHERE price < 10 OR price > 100;
 
+2605
+SELECT
+  p.name,
+  pr.name
+FROM
+  products AS p
+JOIN
+  providers AS pr ON p.id_providers = pr.id
+JOIN
+  categories AS c ON p.id_categories = c.id
+WHERE
+  c.name = 'executive';
