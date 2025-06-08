12579140635f28e7eab281c9b17f16b67a912ef9
Luiz Henrique Silva Andrade <Luiz.andradesilva03@gmail.com>
2025-06-07 23:13:33 -0300

    Create SQL.sql

---
+++ SQL/SQL.sql
@@ -0,0 +1,17 @@
+/*
+2602
+SELECT name
+FROM customers
+WHERE state = 'RS'
+
+
+2603
+SELECT name, street
+FROM customers
+WHERE city = 'Porto Alegre'
+
+2604
+SELECT id, name
+FROM products
+WHERE price < 10 OR price > 100;
+
